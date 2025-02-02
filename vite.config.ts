efb64b7b0ded2c94fa4c932107b2fa5825c33169
Anton Lunyov <anlu@spatial.chat>
2025-02-03 03:00:25 +0400

    fix_3: Fixed vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,41 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { configDotenv } from 'dotenv'
 import vue from '@vitejs/plugin-vue'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { join, resolve } from 'path'
+import { loadEnv, defineConfig } from 'vite'
 
-configDotenv({
-  // path: `.env.${process.env.NODE_ENV}`,
-  path: `.env.dev`,
-})
+type TEnvMode = 'dev' | 'prod'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    port: process.env.SERVER_PORT as unknown as number,
-    host: process.env.SERVER_HOST as string,
-  },
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src'),
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  plugins: [
-    vue(),
-    tsconfigPaths({
-      loose: true,
-      root: './',
-    })
-  ],
+interface IEnvs {
+  H_SERVER_PORT: number
+  H_SERVER_HOST: string
+}
+
+export default defineConfig((conf) => {
+  const root = resolve(__dirname)
+  const mode = conf.mode as TEnvMode
+  const envs = loadEnv(
+    mode,
+    join(root, '/env/'),
+    'H_'
+  ) as unknown as IEnvs
+
+  return {
+    server: {
+      port: envs.H_SERVER_PORT,
+      host: envs.H_SERVER_HOST,
+    },
+    resolve: {
+      alias: {
+        '~': join(root, '/src'),
+        '@': join(root, '/src')
+      }
+    },
+    plugins: [
+      vue(),
+      tsconfigPaths({
+        loose: true,
+        root: root,
+      })
+    ],
+  }
 })
